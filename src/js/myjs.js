92cc8c9cb3252fcdfc0a6be2833b272f48ec7007
DenisRebenock <denmaloyreb@mail.ru>
2020-05-21 17:59:46 +0300

    feat: add wow

--- src/js/myjs.js
+++ src/js/myjs.js
@@ -1,35 +1,10 @@
-document.querySelector(".hamburger-mobile").addEventListener("click", function() {
-    document.querySelector(".mobile-menu").style.left = "0";
+wow = new WOW({
+
+    boxClass: 'wow', // default
+    animateClass: 'animated', // default
+    offset: 0, // default
+    mobile: false, // default
+    live: true // default
 
 })
-
-
-document.querySelector(".close-btn").addEventListener("click", function() {
-    document.querySelector(".mobile-menu").style.left = "-100vw";
-
-})
-
-document.querySelector(".oneclose").addEventListener("click", function() {
-    document.querySelector(".mobile-menu").style.left = "-100vw";
-
-})
-
-document.querySelector(".twoclose").addEventListener("click", function() {
-    document.querySelector(".mobile-menu").style.left = "-100vw";
-
-})
-
-document.querySelector(".threeclose").addEventListener("click", function() {
-    document.querySelector(".mobile-menu").style.left = "-100vw";
-
-})
-
-document.querySelector(".fourclose").addEventListener("click", function() {
-    document.querySelector(".mobile-menu").style.left = "-100vw";
-
-})
-
-document.querySelector(".header__contact-button-mobile").addEventListener("click", function() {
-    document.querySelector(".mobile-menu").style.left = "-100vw";
-
-})
+wow.init();
